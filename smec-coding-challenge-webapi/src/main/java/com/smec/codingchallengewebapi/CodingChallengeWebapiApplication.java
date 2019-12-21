24703cd0e9a9eb0f61a77a8acbc229812ce260bc
Stefan Steiner <dienstfuchs@gmail.com>
2019-12-21 08:02:36 +0100

    initial web api

---
+++ smec-coding-challenge-webapi/src/main/java/com/smec/codingchallengewebapi/CodingChallengeWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.smec.codingchallengewebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallengeWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingChallengeWebapiApplication.class, args);
+	}
+
+}
