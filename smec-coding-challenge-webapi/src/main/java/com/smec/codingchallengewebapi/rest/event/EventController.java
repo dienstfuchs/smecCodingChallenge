07e516effcdbdc24f5e8c0c85e60552616d74fd9
Stefan Steiner <dienstfuchs@gmail.com>
2020-01-04 20:17:46 +0100

    logging in EventController

--- smec-coding-challenge-webapi/src/main/java/com/smec/codingchallengewebapi/rest/event/EventController.java
+++ smec-coding-challenge-webapi/src/main/java/com/smec/codingchallengewebapi/rest/event/EventController.java
@@ -3,6 +3,8 @@ package com.smec.codingchallengewebapi.rest.event;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.HttpStatus;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EventController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	private final EventService eventService;
 
 	public EventController(EventService eventService) {
@@ -28,18 +32,21 @@ public class EventController {
 	public List<EventDTO> getAllEvents(@PathVariable String accountName,
 			@RequestParam @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime startDate,
 			@RequestParam @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime endDate) {
+		logger.info("getAllEvents() executed");
 		return eventService.getAllEventsByAccountName(accountName, startDate, endDate);
 	}
 
 	@PostMapping("/accounts/{accountName}/events")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	EventDTO createEvent(@RequestBody EventDTO event, @PathVariable String accountName) {
+		logger.info("createEvent(Account: '{}', Event: '{}') executed", accountName, event);
 		return eventService.createEvent(event, accountName);
 	}
 	
 	@DeleteMapping("/accounts/deleteOldEvents")
 	@ResponseStatus(code = HttpStatus.OK)
 	void deleteOldEvents() {
+		logger.info("deleteOldEvents() executed");
 		eventService.deleteOldEvents();
 	}
 
