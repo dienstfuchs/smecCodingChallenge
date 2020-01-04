a00b9673b65a0e54a5b65aa1173dda975c4fbaaa
Stefan Steiner <dienstfuchs@gmail.com>
2020-01-04 16:23:33 +0100

    add deployment for server app in kubernetes and simple logging

--- smec-coding-challenge-webapi/src/main/java/com/smec/codingchallengewebapi/rest/account/AccountController.java
+++ smec-coding-challenge-webapi/src/main/java/com/smec/codingchallengewebapi/rest/account/AccountController.java
@@ -2,6 +2,8 @@ package com.smec.codingchallengewebapi.rest.account;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/accounts")
 public class AccountController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	private final AccountService accountService;
 
 	public AccountController(AccountService accountService) {
@@ -24,17 +28,20 @@ public class AccountController {
 
 	@GetMapping
 	public List<AccountDTO>  getAllAccounts() {
+		logger.info("getAllAccounts() executed");
 		return accountService.findAll();
 	}
 
 	@PostMapping()
 	@ResponseStatus(code = HttpStatus.CREATED)
 	AccountDTO createAccount(@RequestBody AccountDTO account) {
+		logger.info("createAccount({})executed", account.getName());
 		return accountService.createAccount(account);
 	}
 
 	@PutMapping("/{accountName}")
 	AccountDTO updateAccount(@RequestBody AccountDTO newAccount, @PathVariable String accountName) {
+		logger.info("updateAccount({}) for account '{}' executed", newAccount.getName(), accountName);
 		return accountService.updateAccount(accountName, newAccount);
 	}
 
